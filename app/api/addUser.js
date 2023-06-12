26f416f1ace1c8540e859f428d1d326e6f6132e5
umermemon4648 <umermemon4648@gmail.com>
2023-06-12 07:02:59 +0500

    update user end point  created

--- app/api/addUser.js
+++ app/api/addUser.js
@@ -8,7 +8,7 @@ export default async function handler(req, res){
         const {fname, lname, email, gender} = req.body
 
         try {
-            const snapshot = await addDoc(collection(firestore,'users-details'),{fname, lname, email, gender})
+            const snapshot = await addDoc(collection(firestore,'users-info'),{fname, lname, email, gender})
             res.status(200).json({id: snapshot.id, fname, lname, email, gender})
         } catch (error) {
             res.status(500).json({error: "Failed to add document"})
---
+++ app/api/updateUser.js
@@ -0,0 +1,23 @@
+import {updateDoc, getFirestore, collection} from 'firebase/firestore'
+import app from '../firebase/config'
+const firestore = getFirestore(app)
+
+export default async function handler(req, res){
+    if (req.method==='PUT') {
+        const {id, fname, lname, email, gender} = req.body
+        try {
+
+            const updatedSnapshot = await updateDoc(collection(firestore, 'users-info',id), 
+            {fname, lname, email, gender}
+            )
+            res.status(200).json({updatedSnapshot})
+        } catch (error) {
+            res.status(500).json({error:'Failed to update document'})
+        }
+    }
+    
+    else{
+        res.status(405).json({error:'This Method is not allowed'})
+    }
+
+}
