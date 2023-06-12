738c7462af5caa46cc25d75997b274ee06559a23
umermemon4648 <umermemon4648@gmail.com>
2023-06-12 06:48:35 +0500

    add new user end point  created

---
+++ app/api/addUser.js
@@ -0,0 +1,21 @@
+import {addDoc, getFirestore, collection} from 'firebase/firestore'
+import app from '../firebase/config'
+
+const firestore = getFirestore(app)
+
+export default async function handler(req, res){
+    if (req.method==='POST') {
+        const {fname, lname, email, gender} = req.body
+
+        try {
+            const snapshot = await addDoc(collection(firestore,'users-details'),{fname, lname, email, gender})
+            res.status(200).json({id: snapshot.id, fname, lname, email, gender})
+        } catch (error) {
+            res.status(500).json({error: "Failed to add document"})
+        }
+    }
+    else{
+        
+        res.status(405).json({error: "Method not allowd"})
+    }
+}
